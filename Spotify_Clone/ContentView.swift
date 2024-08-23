c47d523d7f8d1746e152e5c8a79e4af732ccf8bb
A.F. Adib <a.f.adib36@gmail.com>
2024-08-23 22:52:10 +0600

    Initial commit

---
+++ Spotify_Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 23/8/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world")
+            Image(systemName: "heart.fill")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify_Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 23/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
