db9ab73ea8aa0827d08164089fc7a8dd8b514eb7
A.F. Adib <a.f.adib36@gmail.com>
2024-08-23 23:32:15 +0600

    updated

--- Spotify_Clone/ContentView.swift
+++ Spotify_Clone/ContentView.swift
@@ -11,11 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world")
-            Image(systemName: "heart.fill")
+            
+            
         }
         .padding()
     }
