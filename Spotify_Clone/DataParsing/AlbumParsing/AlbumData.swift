fcac09f775a0873f9925ae7a6ce3937d020ee837
A.F. Adib <a.f.adib36@gmail.com>
2024-08-28 03:35:25 +0600

    album json

---
+++ Spotify_Clone/DataParsing/AlbumParsing/AlbumData.swift
@@ -0,0 +1,12 @@
+//
+//  AlbumData.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 28/8/24.
+//
+
+import Foundation
+
+struct Album: Hashable, Codable {
+    
+}
---
+++ Spotify_Clone/Home/NewItems.swift
@@ -0,0 +1,25 @@
+//
+//  NewItems.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 28/8/24.
+//
+
+import SwiftUI
+
+struct NewItems: View {
+    var body: some View {
+        HStack{
+            ZStack(alignment: .leading) {
+                Color(.darkGray)
+                HStack{
+                    Image(systemName: "globe")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NewItems()
+}
