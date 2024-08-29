c26eefc7e079866cef08d13fe8a13e71a00ef732
A.F. Adib <a.f.adib36@gmail.com>
2024-08-29 23:58:02 +0600

    modified

--- Spotify_Clone/Home/PodCastList.swift
+++ Spotify_Clone/Home/PodCastList.swift
@@ -12,26 +12,7 @@ struct PodCastList: View {
     var podCasts : PodCast
     
     var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea()
-            VStack(alignment: .leading){
-                Image(podCasts.img)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 150)
-                Text(podCasts.podcast)
-                    .foregroundColor(.white)
-                    .font(.system(size: 17))
-                    .fontWeight(.semibold)
-                    .padding(.top, 2)
-                Text("Show · \(podCasts.network)")
-                    .foregroundColor(.white)
-                    .font(.system(size:13))
-                    .padding(.top, 1)
-            }
-            .frame(width: 150)
-        }
+        
     }
 }
 
