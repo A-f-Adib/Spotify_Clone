c6e8406866fba1e42128e0bc569c2eaed91c6e75
A.F. Adib <a.f.adib36@gmail.com>
2024-09-14 02:38:55 +0600

    isles json added

---
+++ Spotify_Clone/DataParsing/AlbumDetails/IslesData.swift
@@ -0,0 +1,13 @@
+//
+//  IslesData.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 14/9/24.
+//
+
+import Foundation
+
+struct AlbumDetails : Hashable, Codable {
+    var title: String
+    var artists: String
+}
