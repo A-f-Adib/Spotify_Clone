b987f6c2b087972a88f32a160dbf06aa0f71a459
A.F. Adib <a.f.adib36@gmail.com>
2024-09-14 02:29:56 +0600

    homeView added

---
+++ Spotify_Clone/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 14/9/24.
+//
+
+import SwiftUI
+
+let AlbumImageList = ["isles", "together", "tellme", "enough", "blackp", "nopressure"]
+
+let AlbumTitleList = ["Isles", "Together", "Tell Me", "Enough to Believe", "Black Panther", "No Pressure"]
+
+let AlbumArtistList = ["Jon Gurd", "Tinlicker", "Bob Moses", "Various Artists", "Logic"]
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
