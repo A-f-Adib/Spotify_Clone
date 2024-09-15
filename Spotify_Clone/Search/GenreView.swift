7f78319048ca775cca8fc0aaff23caaa8d72b7d3
A.F. Adib <a.f.adib36@gmail.com>
2024-09-15 22:15:02 +0600

    genre added

--- Spotify_Clone/Search/GenreView.swift
+++ Spotify_Clone/Search/GenreView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct GenreView: View {
+    var playlists : PlayList
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack (alignment: .leading){
+//            LinearGradient(
+//                gradient: Gradient(colors: [random()]),
+//                startPoint: .leading, endPoint: .trailing)
+            HStack(alignment: .top){
+                Text(playlists.genre)
+                    .font(.system(size: 14))
+                    .kerning(0)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.leading, 10)
+                    .padding(.top, -15)
+                Spacer()
+                Image(playlists.img)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .rotationEffect(.degrees(25))
+                    .padding(.trailing, -18)
+                    .padding(.bottom, -20)
+            }
+        }.frame(width: 120, height: 70)
+        .clipShape(Rectangle())
+        .cornerRadius(5)
     }
 }
 
 #Preview {
-    GenreView()
+    GenreView(playlists: playlists[0])
 }
