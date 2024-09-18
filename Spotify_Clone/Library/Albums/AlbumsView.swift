26ae8acf32352fb889b552aa833f54f74f85d891
A.F. Adib <a.f.adib36@gmail.com>
2024-09-18 23:51:39 +0600

    AlbumsView added

---
+++ Spotify_Clone/Library/Albums/AlbumsView.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumsView.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 18/9/24.
+//
+
+import SwiftUI
+
+struct AlbumsView: View {
+    @State var srch:String = ""
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumsView()
+}
