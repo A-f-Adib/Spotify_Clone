e1e3aa93fa4c7fd2c3d30edd16b6c96bddfd8f22
A.F. Adib <a.f.adib36@gmail.com>
2024-09-18 23:57:33 +0600

    ArtistsView added

---
+++ Spotify_Clone/Library/Artists/ArtistsView.swift
@@ -0,0 +1,21 @@
+//
+//  ArtistsView.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 18/9/24.
+//
+
+import SwiftUI
+
+struct ArtistsView: View {
+    
+    @State var srch: String = ""
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistsView()
+}
