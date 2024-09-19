8b1e96634a63473ac4e343e351ec3ed0ee49c7de
A.F. Adib <a.f.adib36@gmail.com>
2024-09-19 23:56:27 +0600

    episodesView

--- Spotify_Clone/Library/Episodes/EpisodeView.swift
+++ Spotify_Clone/Library/Episodes/EpisodeView.swift
@@ -12,7 +12,31 @@ struct EpisodeView: View {
     @State var srch: String = ""
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            ScrollView(.vertical, showsIndicators: false)
+            {
+                VStack(alignment: .leading){
+                    NewEpisodes(podcasts: podcasts[1])
+                    NewEpisodes(podcasts: podcasts[4])
+                    NewEpisodes(podcasts: podcasts[2])
+                    
+                    Text("Today")
+                        .font(.subheadline)
+                        .foregroundColor(.white).bold()
+                        .opacity(0.6)
+                        .padding(.init(top: 10, leading: 0, bottom: 10, trailing: 0))
+                                        
+                    ForEach (0..<5){i in
+                        EpisodeRow(podcasts: podcasts[i])
+                    }
+                    
+                    Spacer()
+                }
+            }
+        }.foregroundColor(.white)
     }
 }
 
