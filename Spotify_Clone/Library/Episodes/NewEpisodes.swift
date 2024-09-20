50c56d79715afcd2f6491db0290d4efa1984e695
A.F. Adib <a.f.adib36@gmail.com>
2024-09-20 23:50:34 +0600

    modified

--- Spotify_Clone/Library/Episodes/NewEpisodes.swift
+++ Spotify_Clone/Library/Episodes/NewEpisodes.swift
@@ -13,7 +13,30 @@ struct NewEpisodes: View {
     let prgress = CGFloat.random(in: 0 ... 30)
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image(podcasts.img)
+                .resizable()
+                .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .cornerRadius(5)
+                .padding(10)
+            VStack(alignment: .leading, spacing: 5){
+                Text(podcasts.episode)
+                    .font(.system(size: 12, weight: .bold))
+                HStack{
+                    Text("\(podcasts.time) • \(podcasts.timeleft) min left")
+                        .font(.system(size: 10))
+                        .kerning(-0.2)
+                        .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    ZStack(alignment: .leading){
+                        Rectangle()
+                            .frame(width: 100, height: 4)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                            .opacity(0.2)
+                    }
+                }
+            }
+        }
     }
 }
 
