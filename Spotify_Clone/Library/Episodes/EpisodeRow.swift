45a1502b0d5bbca55916a0b8006f653aec85762e
A.F. Adib <a.f.adib36@gmail.com>
2024-09-20 23:55:56 +0600

    EpisodeRow added

---
+++ Spotify_Clone/Library/Episodes/EpisodeRow.swift
@@ -0,0 +1,21 @@
+//
+//  EpisodeRow.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 20/9/24.
+//
+
+import SwiftUI
+
+struct EpisodeRow: View {
+    
+    var podcasts: PodCast
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeRow(podcasts: podcasts[0])
+}
