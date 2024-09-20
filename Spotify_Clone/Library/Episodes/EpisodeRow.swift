12d1fd05af335410aaba283179752df736920358
A.F. Adib <a.f.adib36@gmail.com>
2024-09-20 23:57:40 +0600

    modified

--- Spotify_Clone/Library/Episodes/EpisodeRow.swift
+++ Spotify_Clone/Library/Episodes/EpisodeRow.swift
@@ -12,7 +12,48 @@ struct EpisodeRow: View {
     var podcasts: PodCast
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading){
+            
+            HStack{
+                Image(podcasts.img)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
+                    .cornerRadius(5)
+                VStack(alignment: .leading, spacing: 5){
+                    Text(podcasts.episode)
+                        .font(.system(size: 15, weight: .bold))
+                        .kerning(-0.5)
+                    Text(podcasts.podcast)
+                        .font(.system(size: 12))
+                        .opacity(0.7)
+                }
+            }
+            Text(podcasts.desc)
+                .font(.system(size: 12, weight: .bold))
+                .lineSpacing(3.6)
+                .frame(height: 50)
+                .opacity(0.7)
+            Text("\(podcasts.time) • \(podcasts.duration) min")
+                .font(.system(size: 13, weight: .bold))
+                .opacity(0.7)
+            
+            HStack(spacing: 15){
+                Image(systemName: "checkmark")
+                Image(systemName: "plus.circle")
+                Image(systemName: "arrow.down.circle")
+                Image(systemName: "ellipsis")
+                    .rotationEffect(.degrees(90))
+                Spacer()
+                Image(systemName: "play.circle.fill")
+                    .font(.system(size: 25))
+            }.font(.system(size: 20))
+            .padding([.top,.bottom], 5)
+            Rectangle()
+                .frame(height: 0.5)
+                .opacity(0.4)
+        }.padding(.bottom, 10)
     }
 }
 
