b3fb6b6ceadbc2e97b76267c8aca3328d70178d7
A.F. Adib <a.f.adib36@gmail.com>
2024-09-21 23:42:36 +0600

    modified

--- Spotify_Clone/Library/Downloads/DownloadsView.swift
+++ Spotify_Clone/Library/Downloads/DownloadsView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct DownloadsView: View {
+    
     var body: some View {
-        HStack {
-            Text("Tap")
-                .kerning(-0.2)
-            Image(systemName: "arrow.down.circle")
-            VStack {
-                Text("on an episode to listen without a connection")
-                    .kerning(-0.2)
+        
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack(alignment: .center){
+                Text("No downloads yet")
+                    .font(.system(size: 25)).bold()
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Text("Tap")
+                        .kerning(-0.2)
+                    Image(systemName: "arrow.down.circle")
+                    VStack {
+                        Text("on an episode to listen without a connection")
+                            .kerning(-0.2)
+                    }
+                }.foregroundColor(.white)
+                .font(.system(size: 12))
+                .padding(.top, 3)
+                .padding(.bottom)
+                Button("BROWSE") {
+                    
+                }
+                .padding([.top, .bottom], 10)
+                .padding([.leading, .trailing], 25)
+                .font(.system(size: 13, weight: .semibold))
+                .foregroundColor(.black)
+                .background(Color.white)
+                .cornerRadius(15)
+                
             }
-        }.foregroundColor(.white)
-        .font(.system(size: 12))
-        .padding(.top, 3)
-        .padding(.bottom)
+        }
     }
 }
 
