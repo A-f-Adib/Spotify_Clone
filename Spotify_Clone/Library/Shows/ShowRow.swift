96d3c0e33a470712fbe2f486fe0de268c989cf77
A.F. Adib <a.f.adib36@gmail.com>
2024-09-21 23:46:48 +0600

    showRow added

---
+++ Spotify_Clone/Library/Shows/ShowRow.swift
@@ -0,0 +1,21 @@
+//
+//  ShowRow.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 21/9/24.
+//
+
+import SwiftUI
+
+struct ShowRow: View {
+    
+    var podcasts: PodCast
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowRow(podcasts: podcasts[0])
+}
