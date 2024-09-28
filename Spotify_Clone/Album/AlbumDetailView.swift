e0c8041eec407588c31d14abe295a8b3886e3d29
A.F. Adib <a.f.adib36@gmail.com>
2024-09-29 03:09:31 +0600

    modified

--- Spotify_Clone/Album/AlbumDetailView.swift
+++ Spotify_Clone/Album/AlbumDetailView.swift
@@ -7,34 +7,38 @@
 
 import SwiftUI
 
-var btnBack : some View { Button(action: {
+struct AlbumDetailView: View {
     
-    self.presentationMode.wrappedValue.dismiss()
-}) {
-    VStack(alignment: .trailing) {
-        HStack {
-            Image(systemName: "chevron.backward")
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(.white)
-            Spacer(minLength: 330)
-            Image(systemName: "heart")
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(.white)
-            Image(systemName: "ellipsis")
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(.white)
-                .rotationEffect(.degrees(90))
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var albumdetails: AlbumDetails
+    var albums: Album
+    var btnBack : some View { Button(action: {
+          
+        self.presentationMode.wrappedValue.dismiss()
+    }) {
+        VStack(alignment: .trailing) {
+            HStack {
+                Image(systemName: "chevron.backward")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                Spacer(minLength: 330)
+                Image(systemName: "heart")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                Image(systemName: "ellipsis")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                    .rotationEffect(.degrees(90))
+            }
         }
     }
-}.navigationBarHidden(false)
-}
-
-struct AlbumDetailView: View {
+    }
+         
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AlbumDetailView()
+    AlbumDetailView(albumdetails: <#AlbumDetails#>, albums: <#Album#>)
 }
