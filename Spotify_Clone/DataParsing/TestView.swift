391911cb8e65524879f5e0eb41f0db7136378a72
A.F. Adib <a.f.adib36@gmail.com>
2024-10-01 02:21:30 +0600

    added

---
+++ Spotify_Clone/DataParsing/TestView.swift
@@ -0,0 +1,19 @@
+//
+//  TestView.swift
+//  Spotify_Clone
+//
+//  Created by A.F. Adib on 1/10/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+   
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
