12c423441587bc704ffaee0a711914f815f36f64
A.F. Adib <a.f.adib36@gmail.com>
2024-10-01 02:22:29 +0600

    modified

--- Spotify_Clone/DataParsing/TestView.swift
+++ Spotify_Clone/DataParsing/TestView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct TestView: View {
    
     var body: some View {
+        
+        ZStack{
+        Color.black
+            .ignoresSafeArea()
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+        }
     }
 }
 
